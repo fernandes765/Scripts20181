ff794e30c2b4d503d674b8d4988dd552aeb59cc6
fernandes765 <37230133+fernandes765@users.noreply.github.com>
2018-03-11 00:41:22 -0300

    Add files via upload

---
+++ RAIO.py
@@ -0,0 +1,8 @@
+raio = float(input())
+area = 3.14159 * (raio ** 2)
+print("A=",round(area,4));
+
+R = float(input())
+pi = 3.14159
+area = float(pi)*(R **2)
+print('A=%.4f'%area);
---
+++ Salário.py
@@ -0,0 +1,6 @@
+fun = int(input())
+horas = int(input())
+valor = float(input())
+salary = horas * valor
+print("NUMBER","=",fun);
+print("SALARY","=","U$ %.2f"%salary);
